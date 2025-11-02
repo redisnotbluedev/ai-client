344023c1d66c51688c2cb9bc38b8ee9309299aa7
redisnotbluedev <ededmaude@gmail.com>
2025-11-02 15:47:59 +1030

    rong element

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("input");
+const send = document.getElementById("send");
 input.addEventListener("input", event => {
-    const text = input.textContent.trim();
-    input.disabled = (text === "" || text == "\n");
+	const text = input.textContent.trim();
+	send.disabled = (text === "" || text == "\n");
 });
