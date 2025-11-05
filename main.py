1ecf92d82d244254e27f00b4312b9e1b1c0b14c5
redisnotbluedev <ededmaude@gmail.com>
2025-11-05 17:04:53 +1030

    basic flask app

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, request, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "uploads"
+
+def allowed_file(filename):
+    return "." in filename
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        return "No file part", 400
+    file = request.files["file"]
+    if file.filename == "":
+        return "No selected file", 400
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(path)
+        return f"""{{"path": "{path}"}}""", 201
+    
+    return "Invalid request", 400
+
+@app.route("/uploads/<filename>")
+def uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+    app.run(debug=True)
